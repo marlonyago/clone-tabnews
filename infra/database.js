c91f331ec779d6faf8d774f3a69a205d9979d1f9
marlonyago <marlonwebd@gmail.com>
2025-11-19 01:34:35 +0000

    makes database.ja client.connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
@@ -8,17 +8,18 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
 }
 
 export default {
-   query: query,
-}
+  query: query,
+};
